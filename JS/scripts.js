efbd1521530d4df9b1bc3d0569b01f896eb19218
unknown <apalomec229@gmail.com>
2023-02-16 22:41:40 -0600

    cambios

--- JS/scripts.js
+++ JS/scripts.js
@@ -32,22 +32,4 @@ function addItemCarrito(newItem){
 
 function renderCarrito(){
     tbody.innerHTML = ""
-    carrito.map(item => {
-        const tr = document.createElement("tr")
-        tr.classList.add("ItemCarrito")
-        const Content = ""
-        <th scope="row">1</th>
-        <td class="table__productos">
-            <img src="./imagenes/chapulines.jpg" alt="">
-            <h6 class="title"></h6>
-        </td>
-
-        <td class="table__price"><p>$ 5</p></td>
-        <td class="table__cantidad">
-            <input type="number" min="1" value="1">
-            <button class="delete btn btn-danget">X</button>
-        </td>
-    </tr>
-    })
-
 }
